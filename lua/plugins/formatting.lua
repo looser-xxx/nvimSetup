4b4da8c5df6bd290e0b505759523fd71bd5f8c6d
Suprabhat Maurya <itismaurya4769@gmail.com>
2026-02-08 06:44:52 +0530

    feat: enable auto-format on save for python, js, json, and more

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -10,11 +10,20 @@ return {
           lua = { "stylua" },
           cpp = { "clang-format" },
           c = { "clang-format" },
+          python = { "isort", "black" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+          json = { "prettierd", "prettier", stop_after_first = true },
+          html = { "prettierd", "prettier", stop_after_first = true },
+          css = { "prettierd", "prettier", stop_after_first = true },
+          markdown = { "prettierd", "prettier", stop_after_first = true },
         },
         format_on_save = {
           lsp_fallback = true,
           async = false,
-          timeout_ms = 500,
+          timeout_ms = 1000,
         },
       })
 
